8315dcdd7881f5f57086b6e7f40d46707fbe2a8b
Gioele Barabucci <gioele@svario.it>
2026-02-10 20:47:25 +0100

    TTFBitmapFontExporter: Read current time from SOURCE_DATE_EPOCH, if set
    In order to create bit-for-bit reproducible artefacts, it is necessary
    to force the embedded timestamps to a specific value.
    
    The use of the environment variable `SOURCE_DATE_EPOCH` is specified by
    <https://reproducible-builds.org/specs/source-date-epoch/> and used by
    various Linux distributions like Debian, Arch and Nix.

--- main/java/BitsNPicas/src/com/kreative/bitsnpicas/exporter/TTFBitmapFontExporter.java
+++ main/java/BitsNPicas/src/com/kreative/bitsnpicas/exporter/TTFBitmapFontExporter.java
@@ -229,6 +229,13 @@ public class TTFBitmapFontExporter implements BitmapFontExporter {
 	
 	private static final HeadTable makeHeadTable(BitmapFont bf, ThingsToKeepTrackOf a, int ysize) {
 		Calendar now = new GregorianCalendar();
+		// Support reproducible builds by using SOURCE_DATE_EPOCH as the current UNIX time.
+		String sourceDateEpochEnv = System.getenv("SOURCE_DATE_EPOCH");
+		if (sourceDateEpochEnv != null) {
+			long sourceDateEpoch = Long.parseLong(sourceDateEpochEnv);
+			now.setTimeInMillis(sourceDateEpoch * 1000L);
+		}
+
 		double fontVersion;
 		try {
 			String s = bf.getName(Font.NAME_VERSION);
